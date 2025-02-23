cf812637b1170dfd20122906015b50ddb171cddf
Student <student@Admins-MacBook-Pro.local>
2025-02-23 01:32:33 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Food Habits 
+
+## Which food idenity do people most ascribe themselves as?
+There are many things people can identify as when it comes to eating habits. Vegeterian, Pescaterian, etc. It is important to be able to provide food for anybody with any food indentity. I feel it is important to see what types of food a certain gender eats so any kind of advertising for food can be intentional. Any resturuant or cafe can use this data to see just how much of what food they need.
+
+## Dataset
+- Source: https://www.kaggle.com/datasets/sonalishanbhag/dietaryhabitssurvey/data
+- Description: A survey was conducted among various to see which ages and genders have what kind of eating habits and opinions. Poeple from teens to senior citizens were surveyed and asked about their opinions when it came to their diets. 
+- Size: There are about 78 rows that represent each person that was surveyed. There are 26 columns to represent each question they were asked about. 
+- Quality: The survey was done for a medium-sized group of people and was only based off of their opinions.
+
+## Features
+1. Interactive Analysis
+   - Feature 1: Users are abke to see a list of options that go with viewing data on women and their diets.
+   - Feature 2: Users are abke to see a list of options that go with viewing data on men and their diets.
+   
+2. Data Processing
+   - Feature 1: The code takes the entire dataset and only filters out food identities and the female gender.
+   - Feature 2: The code counts up the number of woman in each unique food identity and matches them up.
+   - Feature 3: The code takes the entire dataset and only filters out food identities and the male gender.
+   - Feature 4: The code counts up the number of each man in each unique food identity and matches them up.
+
+3. Visualizations
+   - Type 1: A bar graph of the data is shown with the x-axis being the name of the food identity and the y-axis being the number of women and men that have a certain food identity. Men are attributed the blue colored bar graph and women are attributed the pink color bar graph.
+   - Type 2: A pie chart is shown with the percentage of women in each food identity.
---
+++ Terminal_Analysis/first_project.js
@@ -0,0 +1,68 @@
+const dfd = require("danfojs-node")
+const readline = require('node:readline');
+
+function main() {
+   while(true) {
+      console.log("Please pick one: ")
+      console.log("1. Analyze female food identities")
+      console.log("2. Generate Visualization")
+      console.log("3. Analyze male food identities")
+      console.log("4. Generate Visualization 2")
+      console.log("5. Exit Program")
+      const rl = readline.createInterface({
+         input: process.stdin,
+         output: process.stdout,
+       });
+       rl.question(`Pick a number`, name => {
+         if (name === "1") {
+            console.log(`${gender()}`);
+            rl.close();
+         }
+         else if (name === "2") {
+            console.log(`great choice ${name}!`);
+            rl.close();
+         }
+         else if (name === "3") {
+            console.log(`${gender2()}`);
+            rl.close();
+         }
+         else if (name === "4") {
+            console.log(`nice choice ${name}!`);
+            rl.close();
+         }
+         else if (name === "5") {
+            console.log(`goodbye!`);
+            rl.close();
+         }
+       });
+      break
+   }
+   
+}
+main()
+
+function gender() {
+   dfd.readCSV("/Users/student/Desktop/Engineering-Practicum/Data_project_HTML/Data/Dietary Habits Survey Data.csv")
+      .then(df => {
+         const selectedColumnsDf = df.loc({ columns: ["What would best describe your diet:", "Gender"] });
+         sub_df = selectedColumnsDf.loc({rows: selectedColumnsDf["Gender"].eq("Female")})
+         const grouped = sub_df.groupby(["What would best describe your diet:"]).count();
+         grouped.rename({ "Gender_count": "Number", "What would best describe your diet:": "Identity" }, { inplace: true });
+         grouped.print();
+
+      })
+      .catch(err => console.error(err));
+};
+
+function gender2() {
+   dfd.readCSV("/Users/student/Desktop/Engineering-Practicum/Data_project_HTML/Data/Dietary Habits Survey Data.csv")
+      .then(df => {
+         const selectedColumnsDf = df.loc({ columns: ["What would best describe your diet:", "Gender"] });
+         sub_df = selectedColumnsDf.loc({rows: selectedColumnsDf["Gender"].eq("Male")})
+         const grouped = sub_df.groupby(["What would best describe your diet:"]).count();
+         grouped.rename({ "Gender_count": "Number", "What would best describe your diet:": "Identity" }, { inplace: true });
+         grouped.print();
+
+      })
+      .catch(err => console.error(err));
+};
